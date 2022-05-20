369d0dac5a5c7ecf4ff8b9b1224e7fc16fff5cde
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2022-05-20 13:47:06 +0200

    Bring naive model to new setup

--- models.py
+++ models.py
@@ -6,31 +6,28 @@ import prepare_data
 from layers import LstmSeq, TacotronEncoder, TacotronMelDecoder, TacotronSpecDecoder
 
 @gin.configurable
-class NaiveLstmTTS():
+class NaiveLstmTTS(tf.keras.Model):
     def __init__(self, latent_dims, mel_bins, spec_bins, num_layers):
+        super().__init__()
         self.mel_bins = mel_bins
         self.num_layers = num_layers
 
-        encoder_inputs = keras.Input(shape=(None,), dtype='int64', name='encoder_inputs')
         self.encoder_emb_layer = keras.layers.Embedding(input_dim=(1+prepare_data.num_characters), output_dim=latent_dims)
         self.encoder_lstm = LstmSeq(latent_dims, num_layers)
-
-        _, encoder_states = self.encoder_lstm(self.encoder_emb_layer(encoder_inputs))
-        encoder_state = encoder_states[-1] # last layer states
-
         decoder_inputs = keras.Input(shape=(None, mel_bins), dtype='float32', name='decoder_inputs')
         self.decoder_lstm = LstmSeq(latent_dims, num_layers)
         self.decoder_dense = keras.layers.Dense(mel_bins, name='mel_dense')
-        decoder_outputs, _ = self.decoder_lstm(decoder_inputs, initial_state=[encoder_state]*num_layers)
-        decoder_outputs = self.decoder_dense(decoder_outputs)
-
         self.spec_decoder_lstm = LstmSeq(latent_dims, num_layers)
         self.spec_decoder_dense = keras.layers.Dense(spec_bins, name='spec_dense')
-        spec_decoder_outputs = self.spec_decoder_dense(self.spec_decoder_lstm(decoder_outputs)[0])
 
-        self.model = keras.Model(
-            [encoder_inputs, decoder_inputs], [decoder_outputs, spec_decoder_outputs], name='naive_lstm'
-        )
+    def call(self, inputs):
+        inputs, mel_inputs = inputs
+        _, encoder_states = self.encoder_lstm(self.encoder_emb_layer(inputs))
+        encoder_state = encoder_states[-1] # last layer states
+        decoder_outputs, _ = self.decoder_lstm(mel_inputs, initial_state=[encoder_state]*self.num_layers)
+        mel_outputs = self.decoder_dense(decoder_outputs)
+        spec_outputs = self.spec_decoder_dense(self.spec_decoder_lstm(mel_outputs)[0])
+        return mel_outputs, spec_outputs
 
     def decode(self, encoder_inputs, num_frames):
         _, encoder_states = self.encoder_lstm(self.encoder_emb_layer(encoder_inputs))
